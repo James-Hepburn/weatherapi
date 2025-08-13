a7ada21241133e29c755031f53b9ecf53963be24
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-13 12:33:33 -0600

    WeatherController

--- src/main/java/com/example/weatherapi/controller/WeatherController.java
+++ src/main/java/com/example/weatherapi/controller/WeatherController.java
@@ -1,4 +1,26 @@
 package com.example.weatherapi.controller;
 
+import com.example.weatherapi.service.WeatherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WeatherController {
+    private WeatherService weatherService;
+
+    public WeatherController (WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/")
+    public String home () {
+        return "index";
+    }
+
+    // implement this later
+    @GetMapping("/weather")
+    public String getWeather (Model model) {
+        return "index";
+    }
 }
--- src/main/java/com/example/weatherapi/service/WeatherService.java
+++ src/main/java/com/example/weatherapi/service/WeatherService.java
@@ -1,4 +1,7 @@
 package com.example.weatherapi.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class WeatherService {
 }
